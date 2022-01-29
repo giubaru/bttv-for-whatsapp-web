20a9a35cc45ac4be4730b9ff9e0807434042491c
Giuliano Marinella <giulianobmarinella@gmail.com>
2022-01-29 20:33:33 +0100

    init commit

---
+++ bttv.js
@@ -0,0 +1,55 @@
+/*
+  Based in https://github.com/mdn/webextensions-examples/tree/master/emoji-substitution
+*/
+
+let emoteMap = tmpEmotes;
+
+function replaceText (node) {
+  
+  if (node.nodeName === 'SPAN' && node.classList.contains('copyable-text')) {
+    
+    // Skip textarea nodes due to the potential for accidental submission
+    // of substituted emoji where none was intended.
+    if (node.parentNode &&
+      node.parentNode.nodeName === 'TEXTAREA') {
+        return;
+    }
+
+    let content = node.innerHTML;
+    
+    // Use the emoteMap for replacements.
+    for (let [word, emote] of emoteMap) {
+      content = content.replace(word, `<img src='${emote}'/>`);
+    }
+
+    // Now that all the replacements are done, perform the DOM manipulation.
+    node.innerHTML = content;
+  }
+  else {
+    // This node contains more than just text, call replaceText() on each
+    // of its children.
+    for (let i = 0; i < node.childNodes.length; i++) {
+      replaceText(node.childNodes[i]);
+    }    
+  }
+}
+
+// Start the recursion from the body tag.
+replaceText(document.body);
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+      // This DOM change was new nodes being added. Run our substitution
+      // algorithm on each newly added node.
+      for (let i = 0; i < mutation.addedNodes.length; i++) {
+        const newNode = mutation.addedNodes[i];
+        replaceText(newNode);
+      }
+    }
+  });
+});
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
---
+++ emotes.js
@@ -0,0 +1,39 @@
+const TWITCH_ID = '90075649';
+
+let emotesEndpoints = [
+  `https://api.betterttv.net/3/cached/frankerfacez/users/twitch/${TWITCH_ID}`,
+  `https://api.betterttv.net/3/cached/users/twitch/${TWITCH_ID}`,
+  'https://api.betterttv.net/3/users/5ff8d0610a24220c8e1d86da',
+  'https://api.betterttv.net/3/cached/emotes/global'
+]
+
+let tmpEmotes = new Map();
+
+const CDN_EMOTES = 'https://cdn.betterttv.net/emote';
+
+function parse_emote(emote){
+  let emote_url = `${CDN_EMOTES}/${emote.id}/1x`;
+  if(emote.images){
+    emote_url = emote.images['1x'];
+  }
+  tmpEmotes.set(emote.code, emote_url);
+}
+
+emotesEndpoints.forEach(
+  function(endpoint) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint);
+    xhr.send();
+    xhr.onload = function() {
+      const bttv = JSON.parse(this.response);
+      if(bttv.channelEmotes) {
+        Array.from(bttv.channelEmotes, emote => parse_emote(emote));
+        Array.from(bttv.sharedEmotes, emote => parse_emote(emote));
+      } else {
+        Array.from(bttv, emote => parse_emote(emote));
+      }
+    }
+  }
+)
+
+
